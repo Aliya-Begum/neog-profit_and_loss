bd1f45ae903fe80405512ce7082facad78c6fe32
Aliya-Begum <aliyaansari0604@gmail.com>
2022-10-09 15:40:08 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,23 +3,31 @@ const calculateBtn = document.querySelector('#calculate-btn');
 const outputDiv = document.querySelector('#output-div');
 
 function calculateProfutAndLoss(initialPrice, quantity, currentPrice){
-    if(currentPrice > initialPrice){
-        const profit = (currentPrice - initialPrice)*quantity;
-        const profitPercentage = (profit/initialPrice)*100;
-        outputDiv.innerText = (`hey, the profit is ${profit} and the percetage is ${profitPercentage}%`);
-
-    }else if(initialPrice > currentPrice){
-        const loss = (initialPrice -currentPrice)*quantity;
-        const lossPercentage = (loss/initialPrice)*100;
-        outputDiv.innerText = (`Hey, the loss ${loss} and the percentage is ${lossPercentage}%`);
-
+    if(initialPrice&&quantity&&currentPrice){
+        if(currentPrice > initialPrice){
+            const profit = (currentPrice - initialPrice)*quantity;
+            const profitPercentage = (profit/initialPrice)*100;
+            showOutput(`hey, the profit is ${profit} and the percetage is ${profitPercentage.toFixed(2)}%`);
+    
+        }else if(initialPrice > currentPrice){
+            const loss = (initialPrice -currentPrice)*quantity;
+            const lossPercentage = (loss/initialPrice)*100;
+            showOutput(`Hey, the loss ${loss} and the percentage is ${lossPercentage.toFixed(2)}%`);
+    
+        }else{
+            showOutput("no pain, no gain and no gain, no pain");
+        }
     }else{
-        outputDiv.innerText = "no pain, no gain and no gain, no pain";
+        showOutput("Please Enter all the fields");
     }
+    
 }
 
 function clickHandler(){
     calculateProfutAndLoss(Number(inputValues[0].value), Number(inputValues[1].value ), Number(inputValues[2].value));
 }
 
+function showOutput(msg){
+    outputDiv.innerText = msg;
+}
 calculateBtn.addEventListener('click', clickHandler);
